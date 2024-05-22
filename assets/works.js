106c933794a894d475cfbb94671997115d020de4
Vincent Merah <vincent.merah@gmail.com>
2024-05-22 14:56:12 +0200

    beginning

--- assets/works.js
+++ assets/works.js
@@ -1,2 +1,32 @@
+/*Variables*/
+
 const id = target.dataset.id;
-const works = await fetch("http://localhost:5678/api/works");
+const gallery = document.querySelector(".gallery");
+
+/*Récuperer les données*/
+
+async function getWorks(){
+    const works = await fetch("http://localhost:5678/api/works");
+    return worksJson = await works.json();
+}
+ getWorks();
+
+/*Affichage*/
+
+async function displayWorks(){
+    const arrayWorks = await getWorks();
+    arrayWorks.array.forEach(work => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        img.src = work.imageUrl;
+        figcaption.innerHTML = work.title;
+        figure.classList.add("figure");
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        figcaption.classList.add("figcaption");
+        gallery.appendChild(figure);
+    });
+}
+
+displayWorks();
